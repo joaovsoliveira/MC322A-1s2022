8f32a3e9592edf3d7c95e38695b6e63f0ebd5099
João Víctor <joaovictorso@hotmail.com.br>
2022-04-06 19:11:28 -0300

    corrige metodos mover, virar e apresenta

--- lab03/src/pt/c02oo/s02classe/s03lombriga/AquarioLombriga.java
+++ lab03/src/pt/c02oo/s02classe/s03lombriga/AquarioLombriga.java
@@ -1,127 +1,110 @@
-package src.pt.c02oo.s02classe.s03lombriga;
 
 public class AquarioLombriga {
-    //define os atributos do objeto, considerando a lombriga e o aquario o mesmo objeto
-    int tam_lombriga;
-    int tam_aquario;
-    int pos_inicio;
-    int orientacao; //se for 1, a lombriga cresce para a direita (cabeça para esquerda), 
-                    //se for -1, a lombriga cresce para a esquerda (cabeça para a direita)
+    // define os atributos do objeto, considerando a lombriga e o aquario o mesmo
+    // objeto
+    int tamLombriga;
+    int tamAquario;
+    int posInicio;
+    int orientacao; // se for 1, a lombriga cresce para a direita (cabeça para esquerda),
+                    // se for -1, a lombriga cresce para a esquerda (cabeça para a direita)
 
-    public AquarioLombriga(int tam_aquario, int tam_lombriga, int pos_inicio){
+    AquarioLombriga(int tamAquario, int tamLombriga, int posInicio) {
         this.orientacao = 1;
-        //caso a lombriga seja maior que o aquario, limita o tamanho da lombriga; 
-        if(tam_aquario<tam_lombriga){
-            this.tam_lombriga = tam_aquario;
-            this.tam_aquario = tam_aquario;
+        // caso a lombriga seja maior que o aquario, limita o tamanho da lombriga;
+        if (tamAquario < tamLombriga) {
+            this.tamLombriga = tamAquario;
+            this.tamAquario = tamAquario;
+        } else {
+            this.tamAquario = tamAquario;
+            this.tamLombriga = tamLombriga;
         }
-        else{
-            this.tam_aquario = tam_aquario;
-            this.tam_lombriga = tam_lombriga;
+        // caso a lombriga esteja em uma posição inicial invalida, insere ela na posição
+        // 1
+        if (posInicio > tamAquario || (tamLombriga - 1) > (tamAquario - posInicio)) {
+            this.posInicio = 1;
+        } else {
+            this.posInicio = posInicio;
         }
-        //caso a lombriga esteja em uma posição inicial invalida, insere ela na posição 1
-        if(pos_inicio>tam_aquario || (tam_lombriga-1)>(tam_aquario-pos_inicio)){
-            this.pos_inicio = 1;
-        }
-        else{
-            this.pos_inicio = pos_inicio;
-        }
-        
+
     }
 
-    public void crescer(){
-        //checa qual a orientação da lombriga, para ver para qual lado deve crescer
-        if(orientacao==1){
-            if(((tam_aquario-pos_inicio+1)-tam_lombriga)>0){
-                tam_lombriga++;
+    void crescer() {
+        // checa qual a orientação da lombriga, para ver para qual lado deve crescer
+        if (orientacao == 1) {
+            if (((tamAquario - posInicio + 1) - tamLombriga) > 0) {
+                tamLombriga++;
             }
-        }
-        else{
-            if((pos_inicio-tam_lombriga)>0){
-                tam_lombriga++;
+        } else {
+            if ((posInicio - tamLombriga) > 0) {
+                tamLombriga++;
             }
         }
     }
 
-    public void mover(){
-        //checa se a lombriga esta para esquerda ou para direita
-        if(orientacao==1){
-            if(pos_inicio!=1){
-                pos_inicio--;
-            }
-            else{
-                //se a operação mover estiver no limite, vira de lado
+    void mover() {
+        // checa se a lombriga esta para esquerda ou para direita
+        if (orientacao == 1) {
+            if (posInicio != 1) {
+                posInicio--;
+            } else {
+                // se a operação mover estiver no limite, vira de lado
                 this.virar();
-                pos_inicio = tam_lombriga;
+
             }
-        }
-        else{
-            if((pos_inicio+1)<tam_aquario){
-                pos_inicio++;
-            }
-            else{
+        } else {
+            if ((posInicio) < tamAquario) {
+                posInicio++;
+            } else {
                 this.virar();
-                pos_inicio = tam_aquario - tam_lombriga;
-            }
-        }
-            
-    }
-
-    public void virar(){
-        orientacao = orientacao*-1;
-    }
-
-    public void apresenta(){
-        //só existem 2 casos para a lombriga, ou ela é menor que o aquario ou tem exatamente o tamanho do aquario
-
-        //começando pelo caso que tem exatamente o tamanho do aquario
-        if(tam_aquario==tam_lombriga){
-            if(orientacao==1){
-                System.out.print("O");
-                for(int i=1; i<tam_lombriga; i++){
-                    System.out.print("@");
-                }
-            }
-            else{
-                for(int i=1; i<tam_lombriga; i++){
-                    System.out.print("@");
-                }
-                System.out.print("O");
             }
         }
 
-        //caso em que a lombriga é menor que o aquario
+    }
+
+    void virar() {
         if(orientacao == 1){
-            //"varre" o comprimento do aquario, para valores menores que a posicao da cabeça ou maiores
-            //que o tamanho da lombriga + a posicao da cabeça, imprime "#". Na posição da cabeça, imprime "O". No tamanho da lombriga
-            //imprime "@"
-            for(int i=1; i<tam_aquario+1; i++){
-                if(i<pos_inicio || i>(pos_inicio+tam_lombriga-1)){
-                    System.out.print("#");
+            posInicio = posInicio + tamLombriga - 1;
+        }
+        else{
+            posInicio = posInicio-tamLombriga+1;
+        }
+        orientacao = orientacao * -1;
+    }
+
+    String apresenta() {
+        String aux = "";
+        if (orientacao == 1) {
+            // "varre" o comprimento do aquario, para valores menores que a posicao da
+            // cabeça ou maiores
+            // que o tamanho da lombriga + a posicao da cabeça, imprime "#". Na posição da
+            // cabeça, imprime "O". No tamanho da lombriga
+            // imprime "@"
+            for (int i = 1; i < tamAquario + 1; i++) {
+                if (i < posInicio || i > (posInicio + tamLombriga - 1)) {
+                    aux = aux.concat("#");
                 }
-                if(i>pos_inicio && i<(pos_inicio+tam_lombriga)){
-                    System.out.print("@");
+                if (i > posInicio && i < (posInicio + tamLombriga)) {
+                    aux = aux.concat("@");
                 }
-                if(i==pos_inicio){
-                    System.out.print("O");
+                if (i == posInicio) {
+                    aux = aux.concat("O");
+                }
+            }
+            System.out.print("\n");
+        } else {
+            for (int i = 1; i < tamAquario + 1; i++) {
+                if (i < (posInicio - tamLombriga + 1) || i > posInicio) {
+                    aux = aux.concat("#");
+                }
+                if ((posInicio - tamLombriga + 1) <= i && i < posInicio) {
+                    aux = aux.concat("@");
+                }
+                if (i == posInicio) {
+                    aux = aux.concat("O");
                 }
             }
             System.out.print("\n");
         }
-        else{
-            for(int i=1; i<tam_aquario+1; i++){
-                if(i<(pos_inicio-tam_lombriga+1) || i>pos_inicio){
-                    System.out.print("#");
-                }
-                if((pos_inicio-tam_lombriga+1)<=i && i<pos_inicio){
-                    System.out.print("@"); 
-                }
-                if(i==pos_inicio){
-                    System.out.print("O");
-                }
-            }
-            System.out.print("\n"); 
-        }
+        return aux;
     }
 }
-
