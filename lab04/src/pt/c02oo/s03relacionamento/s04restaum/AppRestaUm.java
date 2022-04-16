42f1994b0977c9d130d2b5152549e0f3630b3e22
João Víctor <joaovictorso@hotmail.com.br>
2022-04-16 14:37:33 -0300

    corrige entrada e saída do tabuleiro na função main

--- lab04/src/pt/c02oo/s03relacionamento/s04restaum/AppRestaUm.java
+++ lab04/src/pt/c02oo/s03relacionamento/s04restaum/AppRestaUm.java
@@ -1,5 +1,6 @@
 package pt.c02oo.s03relacionamento.s04restaum;
 
+
 public class AppRestaUm {
 
    public static void main(String[] args) {
@@ -8,24 +9,42 @@ public class AppRestaUm {
    
    public static void executaJogo(String arquivoEntrada, String arquivoSaida) {
       Toolkit tk = Toolkit.start(arquivoEntrada, arquivoSaida);
-      
+
+      Tabuleiro tabuleiro = new Tabuleiro();
+      //conecta cada peça ao tabuleiro
+      for(int i=0; i<7; i++){
+         for(int j=0; j<7;j++){
+            tabuleiro.getPeca(i, j).conectar(tabuleiro);
+         }
+      }
+
+      char board[][] = new char[7][7];
+      for(int i=0; i<7; i++){
+         for(int j=0; j<7;j++){
+            board[i][j] = tabuleiro.getValor(i, j);
+         }
+      }
+
+      tk.writeBoard("Tabuleiro inicial", board);
+
       String commands[] = tk.retrieveCommands();
       
-      for (int l = 0; l < commands.length; l++)
+      //realiza os comandos da lista
+      for (int l = 0; l < commands.length; l++){
          System.out.println(commands[l]);
-      
-      char board[][] = {
-         {' ', ' ', 'P', 'P', 'P', ' ', ' '},
-         {' ', ' ', 'P', 'P', 'P', ' ', ' '},
-         {'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-         {'P', 'P', 'P', '-', 'P', 'P', 'P'},
-         {'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-         {' ', ' ', 'P', 'P', 'P', ' ', ' '},
-         {' ', ' ', 'P', 'P', 'P', ' ', ' '}
-      };
-            
-      tk.writeBoard("Tabuleiro inicial", board);
-      
+         tabuleiro.movePeca(commands[l]);
+
+         //atualiza a matriz de impressao
+         for(int i=0; i<7; i++){
+            for(int j=0; j<7; j++){
+               board[i][j] = tabuleiro.getValor(i, j);
+            }
+         }
+         
+         String aux = "source: " + commands[l].substring(0,2) + "; target: " + commands[l].substring(3,5);
+         tk.writeBoard(aux, board);
+      }
+
       tk.stop();
    }
 
